5644e24e78e716a4c238148b0c9ee8eea174927e
shizukabot <78878022+shizukabot@users.noreply.github.com>
2021-02-22 19:28:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-Wow
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Shizuka - Free REST APIs</title>
+
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
+
+<link type="text/css" rel="stylesheet" href="https://colorlib.com/etc/404/colorlib-error-404-8/css/style.css" />
+
+
+<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+</head>
+<body>
+<div id="notfound">
+<div class="notfound">
+<div class="notfound-404">
+<h1>A<span>P</span>I</h1>
+</div>
+<p>Documentation</p>
+<p>Creator: ItsmeikyXSec404</p>
+<a href="https://api.shizukaa.xyz">home page</a>
+<br><br>
+<font color="#b3b3b3">Api Status :</font><font color="#00cc00"> Online</font>
+
+</div>
+</div>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+</body>
+</html>
